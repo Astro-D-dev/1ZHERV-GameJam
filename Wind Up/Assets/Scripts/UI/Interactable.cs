2f987026dacf9546036c2d5577fbc28a3e7b0d23
Astro-D-dev <dashercza@gmail.com>
2026-01-04 23:22:21 +0100

    complete rooms (missing garage)

--- Wind Up/Assets/Scripts/UI/Interactable.cs
+++ Wind Up/Assets/Scripts/UI/Interactable.cs
@@ -3,28 +3,33 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
-    [Header("Hover")]
     public string hoverText = "Press E";
-
-    [Header("Objects to Toggle")]
     public List<GameObject> objectsToToggle;
-    public bool toggleSelf = false; 
-
-    [Header("Messages")]
+    public List<GameObject> objectsToDisable;
     public string[] messages;
+    public bool blocked = false;
+    public bool unblockCondition = false;
 
     public void Interact()
     {
+        if (blocked)
+            return;
+
         foreach (GameObject obj in objectsToToggle)
-        {
             if (obj != null)
                 obj.SetActive(!obj.activeSelf);
-        }
 
-        if (toggleSelf)
-            gameObject.SetActive(!gameObject.activeSelf);
+        foreach (GameObject obj in objectsToDisable)
+            if (obj != null)
+                obj.SetActive(false);
 
         if (messages != null && messages.Length > 0)
             PopupManager.Instance.ShowMessages(messages);
+    }
+
+    void Update()
+    {
+        if (blocked && unblockCondition)
+            blocked = false;
     }
 }
